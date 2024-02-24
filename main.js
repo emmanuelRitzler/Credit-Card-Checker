68210505d23dde397e2c9709e57e24edbc8834b2
Emmanuel Ritzler <ritzler.emmanuel@gmail.com>
2024-02-24 15:33:15 +0100

    Add CSS + DOM events

--- main.js
+++ main.js
@@ -1,27 +1,3 @@
-// All valid credit card numbers
-const valid1 = [4, 5, 3, 9, 6, 7, 7, 9, 0, 8, 0, 1, 6, 8, 0, 8]
-const valid2 = [5, 5, 3, 5, 7, 6, 6, 7, 6, 8, 7, 5, 1, 4, 3, 9]
-const valid3 = [3, 7, 1, 6, 1, 2, 0, 1, 9, 9, 8, 5, 2, 3, 6]
-const valid4 = [6, 0, 1, 1, 1, 4, 4, 3, 4, 0, 6, 8, 2, 9, 0, 5]
-const valid5 = [4, 5, 3, 9, 4, 0, 4, 9, 6, 7, 8, 6, 9, 6, 6, 6]
-
-// All invalid credit card numbers
-const invalid1 = [4, 5, 3, 2, 7, 7, 8, 7, 7, 1, 0, 9, 1, 7, 9, 5]
-const invalid2 = [5, 7, 9, 5, 5, 9, 3, 3, 9, 2, 1, 3, 4, 6, 4, 3]
-const invalid3 = [3, 7, 5, 7, 9, 6, 0, 8, 4, 4, 5, 9, 9, 1, 4]
-const invalid4 = [6, 0, 1, 1, 1, 2, 7, 9, 6, 1, 7, 7, 7, 9, 3, 5]
-const invalid5 = [5, 3, 8, 2, 0, 1, 9, 7, 7, 2, 8, 8, 3, 8, 5, 4]
-
-// Can be either valid or invalid
-const mystery1 = [3, 4, 4, 8, 0, 1, 9, 6, 8, 3, 0, 5, 4, 1, 4] // log false
-const mystery2 = [5, 4, 6, 6, 1, 0, 0, 8, 6, 1, 6, 2, 0, 2, 3, 9] // log true
-const mystery3 = [6, 0, 1, 1, 3, 7, 7, 0, 2, 0, 9, 6, 2, 6, 5, 6, 2, 0, 3] // log false
-const mystery4 = [4, 9, 2, 9, 8, 7, 7, 1, 6, 9, 2, 1, 7, 0, 9, 3] // log false
-const mystery5 = [4, 9, 1, 3, 5, 4, 0, 4, 6, 3, 0, 7, 2, 5, 2, 3] // log true
-
-// An array of all the arrays above
-const batch = [valid1, valid2, valid3, valid4, valid5, invalid1, invalid2, invalid3, invalid4, invalid5, mystery1, mystery2, mystery3, mystery4, mystery5]
-
 // Function for checking if a card number is valid or invalid with the Luhn algorithm
 
 function validateCred(arr) {
@@ -87,6 +63,58 @@ function validateCred(arr) {
         }
     }
 }
+const myNumber = [5, 1, 3, 3, 7, 9, 1, 0, 2, 5, 7, 9]
+console.log(validateCred(myNumber));
+
+const button = document.querySelector('#button')
+const message = document.querySelector('#message')
+
+function limitLength(element, maxLength) {
+    if (element.value.length > maxLength) {
+        element.value = element.value.slice(0, maxLength);
+    }
+}
+
+function splitNumber(string) {
+    let numArray = [];
+    for (let i = 0; i < string.length; i++) {
+        numArray.push(parseInt(string[i]));
+    }
+    return numArray;
+}
+
+button.addEventListener('click', function(event) {
+    event.preventDefault
+
+    const creditCardNumberUser = [];
+    const numberUser = document.querySelector('#card-number');
+    console.log(numberUser);
+    const valueUser = numberUser.value;
+    console.log(typeof valueUser);
+    const splitValue = splitNumber(valueUser);
+    console.log(splitValue);
+
+    creditCardNumberUser.push(splitValue);
+    console.log(creditCardNumberUser);
+
+    if (creditCardNumberUser[0].length < 16) {
+        message.innerHTML = "Please check your number again"
+        message.style.display = 'flex';
+    }
+    else {
+        if (validateCred(creditCardNumberUser[0])) {
+            message.innerHTML = "It's all good ✅";
+            message.style.display = 'flex';
+        }
+        else {
+            message.innerHTML = "Your card is not valid ❌";
+            message.style.display = 'flex';
+            console.log(creditCardNumberUser[0]);
+        }
+    }
+
+    
+}); 
 
 // Function for putting the invalid cards into a new array
 
@@ -140,3 +168,4 @@ function idInvalidCardCompanies(arr) {
     }
     return invalidIdCompanies;
 }
+
